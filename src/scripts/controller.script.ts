dabb94f0c94158c9d420e412250aeff25e373cae
t1r <>
2025-08-27 22:30:41 +0300

    add pause logic

--- src/scripts/controller.script.ts
+++ src/scripts/controller.script.ts
@@ -1,7 +1,8 @@
 import * as monarch from 'monarch.monarch';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-interface props {}
+interface props {
+	isPaused: boolean
+}
 
 interface action {
 	pressed: boolean;
@@ -13,11 +14,18 @@ interface message {}
 export function init(this: props): void {
 	msg.post('.', 'acquire_input_focus');
 	msg.post('#', 'start');
+	this.isPaused = false
 }
 
-export function update(this: props, _dt: number): void {}
+export function update(this: props, _dt: number): void {
+}
 
-export function on_input(this: props, _actionId: hash, _action: action): void {}
+export function on_input(this: props, _actionId: hash, _action: action): void {
+	if (_actionId === hash('pause') && _action.pressed) {
+		pprint('----------------- pause -----------------');
+		msg.post('/game#collectionproxy', 'set_time_step', { factor: this.isPaused ? 1 : 0, mode: 0 });
+		this.isPaused = !this.isPaused
+	}}
 
 export function on_message(
 	this: props,
